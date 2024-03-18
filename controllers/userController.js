f0344cb0262d2644f48573919c4224795b0c1f02
Jm Osias <jmosias@outlook.com>
2024-03-19 03:30:30 +0800

    Fixed authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { User } from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
 const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET_TOKEN, { expiresIn: "1d" });
+  return jwt.sign({ _id }, process.env.SECRET_TOKEN, { expiresIn: "30d" });
 };
 
 // POST login
@@ -20,7 +20,7 @@ export const userLogin = async (request, response) => {
 
     response.status(200).json({ message: "Login Successful" });
   } catch (error) {
-    response.status(400).json({ error: error.message });
+    return response.status(400).json({ message: error.message });
   }
 };
 
@@ -33,7 +33,7 @@ export const userRegister = async (request, response) => {
     const token = createToken(user._id);
     response.status(201).json({ email, token });
   } catch (error) {
-    response.status(400).json({ error: error.message });
+    return response.status(400).json({ message: error.message });
   }
 };
 
